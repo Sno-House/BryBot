a2b61811b3727dbe6e1539ac9ec6181f4810e062
Bryan Ngo <bngo@ucdavis.edu>
2017-08-17 18:20:26 -0700

    Summoner Test

--- cogs/LoL.py
+++ cogs/LoL.py
@@ -27,6 +27,23 @@ class Mycog:
             await self.bot.say(spell.keywords)
             await self.bot.say("--------")
 
+    @commands.command()
+    async def getChamps(self, champ):
+        # annie = Champion(name="Annie", region="NA")
+        champion = Champion(name=champ)
+        await self.bot.say(champion.name)
+        await self.bot.say(champion.title)
+        for spell in champion.spells:
+            await self.bot.say(spell.name)
+            await self.bot.say(spell.keywords)
+
+        await self.bot.say(champion.info.difficulty)
+        await self.bot.say(champion.passive.name)
+        await self.bot.say({item.name: count for item, count in champion.recommended_itemsets[0].item_sets[0].items.items()})
+        await self.bot.say(champion._Ghost__all_loaded)
+        await self.bot.say(champion)
+        await self.bot.say("Thank you, come again!")
+
 
 def setup(bot):
     bot.add_cog(Mycog(bot))
